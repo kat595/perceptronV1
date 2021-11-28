a7a2f56457ee07a10d6acb9aa2e060710a226bf2
Patryk Murawski <297077@o365.stud.umk.pl>
2021-11-28 16:22:03 +0100

    Reformat code

--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@
 import PIL.Image
 from PIL import ImageTk
 from tkinter import *
+import matplotlib.pyplot as plt
 from Funkcje import *
 
 
@@ -14,7 +15,156 @@ def on_black(num):  # definicja funkcji zaimplementowanej pozniej
 def clear():  # definicja funkcji zaimplementowanej pozniej
     clear1()
 
+#PRZYKLADY UCZACE
+Zero_grid = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+]
 
+Jeden_grid = [
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 1, 1],
+    [0, 0, 1, 0, 1],
+    [0, 1, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1]
+]
+
+Dwa_grid = [
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1]
+]
+
+Trzy_grid = [
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+]
+
+Cztery_grid = [
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1]
+]
+
+Piec_grid = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+]
+
+Szesc_grid = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+]
+
+Siedem_grid = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1]
+]
+
+Osiem_grid = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+]
+
+Dziewiec_grid = [
+    [1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1]
+]
+#PRZYKLADY UCZACE
+
+#ADALINE
+class Adaline():
+    def __init__(self, no_of_inputs, learning_rate=0.01, iterations=100):
+        self.no_of_inputs = no_of_inputs
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.weights = np.random.random(2 * self.no_of_inputs)  # Zadanie: dopisac bias.
+        self.errors = []
+
+    def train(self, training_data_x, training_data_y, name):
+        training_data_x = self._standarize(training_data_x)
+        for _ in range(self.iterations):
+            e = 0
+            for x, y in zip(training_data_x, training_data_y):  # Zadanie: losowy wybor przykladow uczacych.
+                out = self.output(x)
+                self.weights += self.learning_rate * (y - out) * x
+                e += (y - out) ** 2
+            self.errors.append(e)
+        plt.plot(range(len(self.errors)), self.errors)
+        plt.savefig('learning_curve_' + name + '.pdf')
+        plt.close()
+
+    def _standarize(self, training_data_x):
+        pass
+        # Zadanie: X' = (X - Mean(X))/Std(X)
+
+    def _normalize(self, training_data_x):
+        pass
+        # Zadanie: X' = (X - min(X))/(max(X) - min(X))
+
+    def _activation(x):
+        # Zadanie: dopisac fragment kodu realizujacy
+        # Adaline w przypadku funkcji aktywacji innej niz f(x)=x.
+        return x
+
+    def _activation_derivative(x):
+        return 1
+
+    def output(self, input):
+        inp = np.concatenate([input, fourier_transform(input)])
+        summation = self._activation(np.dot(self.weights, inp))
+        return summation
+#ADALINE
+
+adalines = [Adaline(7 * 5), Adaline(7 * 5), Adaline(7 * 5), Adaline(7 * 5), Adaline(7 * 5), Adaline(7 * 5),
+            Adaline(7 * 5), Adaline(7 * 5), Adaline(7 * 5), Adaline(7 * 5)]
+
+#GUI
 window = Tk()  # instancja okna
 window.geometry("785x850")
 window.title("Perceptron")
@@ -159,6 +309,7 @@ poleG5 = Button(frame, font='Times 20 bold', bg='gray', fg='gray', height=2, wid
 poleG5.grid(row=6, column=4)
 
 window.config(background='#100b40')  # kolor tła okna
+#GUI
 
 
 def on_black1(num):  # zamalowanie wybranego kafelka na czarno
